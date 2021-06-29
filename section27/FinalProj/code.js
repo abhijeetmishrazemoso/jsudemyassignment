f49f86b6e6f9e7195ffd14c0ded3c4e8ddc51fb5
Abhijeet Mishra <abhijeet.mishra@zemosolabs.com>
2021-06-29 18:54:20 +0530

    form assignment done

--- section27/FinalProj/code.js
+++ section27/FinalProj/code.js
@@ -1,8 +1,6 @@
 // Here is a bunch of Lorem Ipsum. You will need it. I recomend using word wrap to view this! View > Toggle Word Wrap
 const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum lorem vel elit laoreet, vitae maximus ligula consectetur. Etiam magna quam, varius at risus eu, rutrum commodo lacus. Maecenas felis sem, consequat non congue non, vestibulum vitae orci. Etiam laoreet ultricies enim, sit amet gravida massa sodales vel. Vivamus tempus neque sed mi elementum ornare eget non lorem. Donec dapibus ex tortor, ut vehicula metus scelerisque eget. Aenean dictum iaculis nulla, vel dapibus justo dignissim quis. Curabitur vitae efficitur neque. Ut vitae orci semper, efficitur dolor eget, porta dui. Cras eu sapien aliquam mauris facilisis ullamcorper. Nullam facilisis sem diam, quis tempor felis interdum ut. Praesent eu nibh arcu. Proin a quam augue. Integer malesuada finibus sem eget consectetur. Aenean non hendrerit purus, quis auctor elit. Duis ullamcorper, ex vitae rutrum ullamcorper, diam justo maximus quam, eget pellentesque metus nunc sit amet arcu. Nunc id eros leo. Praesent ultricies, nunc sit amet finibus posuere, quam magna viverra lectus, non tincidunt tellus mauris sit amet turpis. Nam eleifend quam vel justo pulvinar euismod. Duis ac elit id risus ultricies sollicitudin vitae ac quam. Donec et cursus leo, non dignissim enim. Vestibulum in felis imperdiet, venenatis tortor iaculis, congue augue. Nullam egestas dui nec quam sagittis sollicitudin ac blandit turpis. Nunc in fermentum odio. Morbi sodales mi nibh, ac cursus felis ultricies sed. Quisque at porttitor risus. Aenean a massa in libero venenatis tempus sed in mi. Nullam luctus sapien eget sagittis egestas. Pellentesque eros sem, semper eu molestie in, commodo a lacus. Phasellus pellentesque erat magna, eu facilisis nibh porttitor eget. Ut id mi lobortis, consequat orci ac, vulputate metus. Sed vulputate turpis nec lectus malesuada lacinia. Quisque non mattis eros. Aliquam pellentesque urna vulputate, auctor nibh in, fermentum odio. Suspendisse aliquam neque et aliquam facilisis. Donec sapien felis, molestie vel vestibulum non, sagittis sed felis. Aenean sodales iaculis dolor eget malesuada. Fusce ac libero hendrerit, viverra nunc iaculis, maximus nisi. Nulla volutpat nisi purus, non interdum erat hendrerit vitae. Integer viverra dignissim cursus. Phasellus eu libero sed ex faucibus convallis. In et egestas ipsum. Cras a metus mollis, laoreet risus eget, ultrices purus. Duis at ligula venenatis, imperdiet nulla et, bibendum augue. Pellentesque eleifend libero maximus, pretium lectus et, condimentum metus. Nulla vel est diam. In fermentum ut nulla eget finibus. Aliquam at turpis sagittis, feugiat diam ac, pulvinar quam. Phasellus pretium porta aliquet. Nunc sit amet nisl viverra justo porttitor egestas vel vitae eros. Fusce sit amet fringilla sapien. Nunc placerat, est sit amet laoreet pretium, augue turpis lacinia ipsum, non luctus magna ipsum vel sapien. Fusce luctus odio eget nunc rutrum ullamcorper. Integer vel ligula id elit viverra aliquam. Praesent id ultricies neque. Quisque molestie tellus ac ex vulputate rhoncus. Donec nec massa eget augue tincidunt ultrices vitae at diam. Ut risus purus, egestas eu eros sed, egestas auctor erat. Integer vulputate erat nisi, in aliquet turpis faucibus vel. Praesent neque augue, congue at nunc ut, congue ultricies erat. Ut sed erat in dui ornare malesuada. Nam id elementum ex. Vestibulum condimentum erat quis erat molestie hendrerit. Etiam laoreet volutpat quam, eget ornare ex molestie mollis. Phasellus eros mi, ultrices vel dui ac, facilisis consectetur nunc. Aliquam erat volutpat. Donec rutrum laoreet iaculis.';
 
-// Add a list of colors to this array
-const colors = ['', ''];
 function getBetweenPrefixed() {
   document.getElementById('rand-numb').innerHTML = getRandomNumber();
 }
@@ -51,19 +49,90 @@ function getCurrentTime() {
   const date = new Date();
   document.getElementById('current-time').innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
+function convertInchesToFeet() {
+  // feet = inches / 12;
+  const inches = document.getElementById('inches-to-feet').value;
+  document.getElementById('display-feet').innerHTML = inches / 12;
+}
 function convertFeetToInches() {
   const ft = document.getElementById('feet-to-inches-input').value;
   document.getElementById('display-inches').innerHTML = ft * 12;
 }
-function convertInchesToFeet() {
-  // feet = inches / 12;
-  const inches = document.getElementById('inches-to-feet').value;
-  document.getElementById('display-feet').innerHTML = inches * 12;
+
+function doWordLengthComparison() {
+  const word1 = document.getElementById('word1').value;
+  const word2 = document.getElementById('word2').value;
+  if (word1.length === word2.length) {
+    document.getElementById('word-compare-res').innerHTML = 'Words are equal';
+  } else {
+    document.getElementById('word-compare-res').innerHTML = 'Words are not equal';
+  }
+}
+
+const allChars = 'abcdefigklmnopqrstuvwxyz0123456789';
+function getRandPassword() {
+  const len = 10;
+  let result = '';
+  for (let index = 0; index < len; index++) {
+    result += allChars.charAt(getRandomNumber(0, allChars.length - 1));
+  }
+  document.getElementById('rand-pass-res').innerHTML = result;
+}
+
+function isNoEvenOrOdd() {
+  const number = document.getElementById('even-odd').value;
+  const resultElement = document.getElementById('even-odd-res');
+  const result = number % 2
+    ? ('The no is odd')
+    : ('The no is even');
+  resultElement.innerHTML = result;
+}
+
+const colors = ['green', 'yellow', 'blue', 'purple', 'cyan', 'magenta', 'white', 'coral', 'red', 'lightred', 'lightgrey'];
+
+function getRandomColor() {
+  document.getElementById('random-color-result').innerHTML = colors[getRandomNumber(0, colors.length)];
 }
 // Use this object for your random name generator. Enter in a bunch of objects with first and last names. When generating a new random name, try to grab a random first or last name from any object. Don't just take object as a whole.
 const names = [
   {
-    firstName: '',
-    lastName: '',
+    firstName: 'Rachel',
+    lastName: 'White',
+  },
+  {
+    firstName: 'Heather',
+    lastName: 'Williams',
+  },
+  {
+    firstName: 'Saroka',
+    lastName: 'Chista',
+  },
+  {
+    firstName: 'Capila',
+    lastName: 'Andromeda',
+  },
+  {
+    firstName: 'Kathy',
+    lastName: 'sierra',
+  },
+  {
+    firstName: 'santosh',
+    lastName: 'Reddy',
+  },
+  {
+    firstName: 'Priya',
+    lastName: 'Mehta',
+  },
+  {
+    firstName: 'Sanjay',
+    lastName: 'Bhansali',
   },
 ];
+
+function getRandFirstAndLastName() {
+  const randNo1 = getRandomNumber(0, names.length);
+  const randNo2 = getRandomNumber(0, names.length);
+  const result = `${names[randNo1].firstName} ${
+    names[randNo2].lastName}`;
+  document.getElementById('rand-first-last-res').innerHTML = result;
+}
